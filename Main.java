48561f33007913208ddf5cf7b9d79444b9806041
Dominik <dominik.stefanik@gmail.com>
2019-12-08 23:58:21 +0000

    2nd commit in 2nd branch

--- Main.java
+++ Main.java
@@ -1,5 +1,9 @@
 public class Main {
     public static void main(String[] args) {
         String[] tabelaPracownicy = {"irek", "wacek", "karol"};
+        int i;
+        for(i = 0; i < tabelaPracownicy.length; i++) {
+                System.out.println(tabelaPracownicy[i]);
+            }
     }
 }
